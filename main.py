6b643da5e692f240b4286b353dcadcff850317f1
ghacglogin <git@ghacg.com>
2023-01-10 16:56:14 +0800

    增加功能：保存、加载配置

--- main.py
+++ main.py
@@ -19,37 +19,48 @@ def ui():
     root.iconbitmap("tmp.ico")
     os.remove("tmp.ico")
 
+
+    varengin = StringVar(root)
+    varweight = StringVar(root)
+    varcfg = StringVar(root)
+    varkomi = DoubleVar(root)
+    varh = IntVar(root)
+    varw = IntVar(root)
+    enginlist = scan('Engin','.*.exe')
+    weightlist = scan('Weight','.*')
+    cfglist = scan('Config','.*.cfg')
+    cbox=jsonw.load()
+    varengin.set(cbox['engin'] if 'engin' in cbox.keys() else enginlist[0])
+    varweight.set(cbox['weight'] if 'weight' in cbox.keys() else weightlist[0])
+    varcfg.set(cbox['cfg'] if 'cfg' in cbox.keys() else cfglist[0])
+    varkomi.set(cbox['komi'] if 'komi' in cbox.keys() else 7.5)
+    varw.set(cbox['w'] if 'w' in cbox.keys() else 19)
+    varh.set(cbox['h'] if 'h' in cbox.keys() else 19)
+
+
     enginlb = Label(root,text='引擎')
     enginlb.place(relx=0.1,rely=0.05)
-    engincomb = Combobox(root,values=scan('Engin','.*.exe'))
-    engincomb.current(0)
+    engincomb = Combobox(root, values=enginlist, textvariable=varengin)
     engincomb.place(relx=0.2,rely=0.05,relwidth=0.6)
 
     weightlb = Label(root,text='权重')
     weightlb.place(relx=0.1,rely=0.15)
-    weightcomb = Combobox(root,values=scan('Weight','.*'))
-    weightcomb.current(0)
+    weightcomb = Combobox(root, values=weightlist, textvariable=varweight)
     weightcomb.place(relx=0.2,rely=0.15,relwidth=0.6)
 
     cfglb = Label(root,text='配置文件')
     cfglb.place(relx=0.1,rely=0.25)
-    cfgcomb = Combobox(root,values=scan('Config','.*.cfg'))
-    cfgcomb.current(0)
+    cfgcomb = Combobox(root, values=cfglist ,textvariable=varcfg)
+    #cfgcomb.current(0)
     cfgcomb.place(relx=0.2,rely=0.25,relwidth=0.6)
 
-    varkomi = DoubleVar()
     komilb = Label(root,text='贴目')
     komilb.place(relx=0.1,rely=0.35)
     komihintlb = Label(root,text='可以自行输入')
     komihintlb.place(relx=0.42,rely=0.35)
     komicomb = Combobox(root,textvariable=varkomi,values=[0.0,0.5,2.5,6.5,7.5])
-    komicomb.current(0)
+    #komicomb.current(0)
     komicomb.place(relx=0.2,rely=0.35,relwidth=0.2)
-
-    varw = StringVar(root)
-    varw.set(19)
-    varh = StringVar(root)
-    varh.set(19)
     
     wlb= Label(root, text='棋盘尺寸：宽')
     wlb.place(relx=0.1,rely=0.45)
@@ -66,9 +77,9 @@ def ui():
         cbox['engin']=str(engincomb.get())
         cbox['weight']=str(weightcomb.get())
         cbox['cfg']=str(cfgcomb.get())
-        cbox['komi']=str(komicomb.get())
-        cbox['w']=str(wspinbox.get())
-        cbox['h']=str(hspinbox.get())
+        cbox['komi']=float(komicomb.get())
+        cbox['w']=int(wspinbox.get())
+        cbox['h']=int(hspinbox.get())
         textshow = preview(cbox,1)      #得到显示的字符串
         textprev.config(state=NORMAL)   #恢复可编辑
         textprev.delete('1.0',END)      #清空
@@ -81,7 +92,11 @@ def ui():
         command = preview(cbox,2)
         res = jsonw.set("config.txt",command,cbox["komi"],cbox["w"],cbox["h"])
         if(res==True):
+            jsonw.write(cbox)
             os.system('jre\java17\\bin\javaw.exe -jar lizzie-yzy2.5.1-shaded.jar')#jar方式启动
+
+
+
 
     btnprev = Button(root, text='预览', command=prev)
     btnstart = Button(root, text='启动', command=start)
--- plugins/jsonw.py
+++ plugins/jsonw.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 
@@ -14,3 +15,24 @@ def set(filename, command, komi=7.5, width=19, height=19, name="AutoStart"):
     with open(filename, 'w', encoding='utf-8') as cfg:
         json.dump(config, cfg, ensure_ascii=False, indent=2)
         return True
+
+
+path = 'helper-config'
+def checkdir():
+    if (os.path.exists(path)==False):
+        os.mkdir(path)
+def write(cbox):
+    checkdir()
+    with open(path+'\\config.json','w+', encoding='utf-8') as cfg:
+        json.dump(cbox, cfg, ensure_ascii=False, indent=2)
+    return True
+def load():
+    checkdir()
+    
+    try:
+        with open(path+'\\config.json','r+', encoding='utf-8') as cfg:
+            cbox = json.load(cfg)
+    except:
+        with open(path+'\\config.json','w+', encoding='utf-8') as cfg:
+            return {}
+    return cbox
